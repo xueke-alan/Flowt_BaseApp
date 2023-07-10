2e702ce308b3eb6d5479ac8a78c2e5e53d1a8936
xueke <xueke.alan@outlook.com>
2023-07-10 22:06:34 +0800

    treeshakeing

--- src/views/exception/404.vue
+++ src/views/exception/404.vue
@@ -4,37 +4,37 @@
       <img src="~@/assets/images/exception/404.svg" alt="" />
     </div>
     <div class="text-center">
-      <h1 class="text-base text-gray-500">抱歉，你访问的页面不存在</h1>
+      <h1 class="text-base text-gray-500">抱歉，你访问的页面不存在!</h1>
       <n-button type="info" @click="goHome">回到首页</n-button>
     </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { useRouter } from 'vue-router';
-  const router = useRouter();
-  function goHome() {
-    router.push('/');
-  }
+import { useRouter } from 'vue-router';
+const router = useRouter();
+function goHome() {
+  router.push('/');
+}
 </script>
 
 <style lang="less" scoped>
-  .page-container {
-    width: 100%;
-    border-radius: 4px;
-    padding: 50px 0;
-    height: 100vh;
+.page-container {
+  width: 100%;
+  border-radius: 4px;
+  padding: 50px 0;
+  height: 100vh;
 
-    .text-center {
-      h1 {
-        color: #666;
-        padding: 20px 0;
-      }
-    }
-
-    img {
-      width: 350px;
-      margin: 0 auto;
+  .text-center {
+    h1 {
+      color: #666;
+      padding: 20px 0;
     }
   }
+
+  img {
+    width: 350px;
+    margin: 0 auto;
+  }
+}
 </style>
